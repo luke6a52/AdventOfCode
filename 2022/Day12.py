65b5f44eaaa36a5d750f4ccb009591e96166f58e
luke6a52 <luke6a52@vt.edu>
2022-12-12 08:03:08 -0500

    2022 Day 12 solution by hand

---
+++ 2022/Day12.py
@@ -0,0 +1,5 @@
+with open('2022/Inputs/Day12.txt', 'r') as input_file:
+    lines = [line.rstrip() for line in input_file]
+
+print(f'1. Answer is: 484') # 484   33m 29s (by hand)
+print(f'2. Answer is: 478') # 478   36m (by hand)
