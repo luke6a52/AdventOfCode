46d0486e905405d52b94522e4e69ab572de7ac81
luke6a52 <luke6a52@vt.edu>
2023-12-11 00:50:44 -0500

    Day 11 solution

---
+++ 2023/Day11.py
@@ -0,0 +1,26 @@
+with open('2023/Inputs/Day11.txt', 'r') as input_file:
+    lines = [line.rstrip() for line in input_file]
+
+sum1 = 0
+sum2 = 0
+gals = []
+rows1 = [2] * len(lines)
+cols1 = [2] * len(lines[0])
+rows2 = [1000000] * len(lines)
+cols2 = [1000000] * len(lines[0])
+for i, line in enumerate(lines):
+    for j, char in enumerate(line):
+        if char == '#':
+            gals.append((i,j))
+            rows1[i] = 1
+            cols1[j] = 1
+            rows2[i] = 1
+            cols2[j] = 1
+for x, start in enumerate(gals):
+    for y, end in enumerate(gals[x+1:]):
+        dist1 = sum(rows1[min(start[0],end[0]):max(start[0],end[0])]) + sum(cols1[min(start[1],end[1]):max(start[1],end[1])])
+        sum1 += dist1
+        dist2 = sum(rows2[min(start[0],end[0]):max(start[0],end[0])]) + sum(cols2[min(start[1],end[1]):max(start[1],end[1])])
+        sum2 += dist2
+print(f'1. Answer is: {sum1}') # 9522407
+print(f'2. Answer is: {sum2}') # 544723432977
